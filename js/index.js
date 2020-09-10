72c0ee0a79f555a33fe2df67ccb36a7531b40f04
Stephen Porath - Desktop <poratheon@gmail.com>
2020-09-10 14:32:20 -0400

    Update from desktop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 console.log('Testing 1...2...3..');
+
+let name = 'Stephen';
+console.log('Hello ' + name  + '.  Welcome!');
+
+//Button testing
+const btn = document.querySelector('button');
+const txt = document.querySelector('p');
+
+btn.addEventListener('click', updateBtn);
+
+function updateBtn() {
+    if (btn.textContent === 'Start machine') {
+        btn.textContent = 'Stop machine';
+        txt.textContent = 'The machine has started!';
+        console.log('The button has been pressed');
+    } else {
+        btn.textContent = 'Start machine';
+        txt.textContent = 'The machine is stopped.';
+        console.log('The button has been pressed');
+    }
+}
+
